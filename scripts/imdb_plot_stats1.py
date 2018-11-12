357c11642dc0de7a6a0bba13c5811b524973824c
hagitbenshoshan <hagit@cooladata.com>
2018-11-12 15:05:58 +0200

    Add files via upload

---
+++ scripts/imdb_plot_stats1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 20:09:08 2018
+
+@author: user
+
+Vocabulary_size
+
+"""
+
+ 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+sns.set(style="white", context="talk")
+cc = sns.color_palette('Set1')
+data = pd.read_csv('C:/Users/user/Downloads/imdb.csv') 
+fig, ax = plt.subplots(figsize=(7, 5))
+ 
+x2 = list(data['Vocabulary Size'])
+
+plt.ylim(1, 200) 
+plt.xlim(0, 10000) 
+ 
+
+sns.distplot(x2,hist = True, kde = False,   color="darkcyan",
+                  bins=250 , kde_kws = {'linewidth': 1 },
+                  hist_kws={ 'edgecolor':'black', "rwidth":1,  'alpha':0.5  }) 
+ 
+plt.xlabel('Vocabulary Size')
+plt.ylabel('number of users')
+
+plt.show() 
---
+++ scripts/imdb_plot_stats2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 20:09:08 2018
+
+@author: user
+
+Missing words % in top 1000
+
+"""
+
+# Import the libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+sns.set(style="white", context="talk")
+cc = sns.color_palette('Set1')
+data = pd.read_csv('C:/Users/user/Downloads/imdb.csv') 
+fig, ax = plt.subplots(figsize=(7, 5))
+ 
+x2 = list(data['missing words %'])
+
+plt.ylim(1, 80) 
+plt.xlim(0, 100) 
+ 
+
+sns.distplot(x2,hist = True, kde = False,   color="darkcyan",
+                  bins=250 , kde_kws = {'linewidth': 1 },
+                  hist_kws={ 'edgecolor':'black', "rwidth":1,  'alpha':0.5  }) 
+ 
+plt.xlabel('Missing words % in top 1000')
+plt.ylabel('number of users')
+
+plt.show()
+  
